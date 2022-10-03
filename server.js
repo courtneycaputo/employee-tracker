3db7ee8125afdd223063b28d546628faeee13999
Courtney Caputo <courtneycaputo@Courtneys-MacBook-Air.local>
2022-10-03 16:02:44 -0700

    create inquirer prompts for adding new role, department, and employee

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ const mysql = require('mysql2');
 //     console.log(`Connected to the employeemanager_db database.`)
 //   );
 
+console.log(`
+,------------------,
+|                  | 
+|     EMPLOYEE     |
+|     MANAGER      |
+|                  | 
+'------------------'
+`) 
+
+
 const menu = () => {
     inquirer.prompt ([
         {
@@ -39,3 +49,58 @@ const menu = () => {
 }
 
 menu ()
+
+// for adding new employee
+inquirer.prompt ([
+    {
+        type: 'input',
+        name:'employeefirst',
+        message: 'What is the first name of the employee?',
+        
+    },
+    {
+        type: 'input',
+        name: 'employeelast',
+        message: 'What is the last name of the employee?',
+    },
+    {
+        type: 'input',
+        name: 'employeerole',
+        message: 'What is the employees role?',
+    },
+    {
+        type: 'input',
+        name: 'employeemanager',
+        message: 'Who is the employees manager',
+    },
+])
+
+// for adding new role
+inquirer.prompt ([
+    {
+        type: 'input',
+        name:'roletitle',
+        message: 'What is the role title?',
+        
+    },
+    {
+        type: 'input',
+        name: 'rolesalary',
+        message: 'What is the salary of this role?',
+    },
+    {
+        type: 'input',
+        name: 'roledepartment',
+        message: 'What department is this role in?',
+    },
+])
+
+// for adding new department
+inquirer.prompt ([
+    {
+        type: 'input',
+        name:'department',
+        message: 'What is the name of this department?',
+        
+    },
+])
